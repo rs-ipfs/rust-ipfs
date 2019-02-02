31ae0162fa130bb2c2e956f8c76e144ba425c603
David Craven <david@craven.ch>
2019-02-02 16:25:44 +0100

    Update to libp2p 0.3.0.

--- src/p2p/behaviour.rs
+++ src/p2p/behaviour.rs
@@ -3,15 +3,64 @@ use libp2p::core::muxing::{StreamMuxerBox, SubstreamRef};
 use libp2p::kad::Kademlia;
 use std::sync::Arc;
 
+/// IPFS bootstrap nodes.
+const BOOTSTRAP_NODES: &[(&'static str, &'static str)] = &[
+    (
+        "QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+        "/ip4/104.131.131.82/tcp/4001",
+    ),
+    (
+        "QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
+        "/ip4/104.236.179.241/tcp/4001",
+    ),
+    (
+        "QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
+        "/ip4/104.236.76.40/tcp/4001",
+    ),
+    (
+        "QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
+        "/ip4/128.199.219.111/tcp/4001",
+    ),
+    (
+        "QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
+        "/ip4/178.62.158.247/tcp/4001",
+    ),
+    /*(
+        "QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
+        "/ip6/2400:6180:0:d0::151:6001/tcp/4001",
+    ),
+    (
+        "QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
+        "/ip6/2604:a880:1:20::203:d001/tcp/4001",
+    ),
+    (
+        "QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
+        "/ip6/2604:a880:800:10::4a:5001/tcp/4001",
+    ),
+    (
+        "QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
+        "/ip6/2a03:b0c0:0:1010::23:1001/tcp/4001",
+    ),*/
+];
+
 /// Behaviour type.
 pub type TBehaviour = Kademlia<SubstreamRef<Arc<StreamMuxerBox>>>;
 
-/// Create a Kademlia behaviour.
-pub fn build_behaviour(peer_id: PeerId) -> TBehaviour {
+/// Create a Kademlia behaviour with the IPFS bootstrap nodes.
+pub fn build_behaviour(local_peer_id: PeerId) -> TBehaviour {
     // Note that normally the Kademlia process starts by performing lots of
     // request in order to insert our local node in the DHT. However here we use
     // `without_init` because this example is very ephemeral and we don't want
     // to pollute the DHT. In a real world application, you want to use `new`
     // instead.
-    Kademlia::without_init(peer_id)
+    let mut behaviour = Kademlia::without_init(local_peer_id);
+
+    for (identity, location) in BOOTSTRAP_NODES {
+        behaviour.add_address(
+            &identity.parse().unwrap(),
+            location.parse().unwrap(),
+        );
+    }
+
+    behaviour
 }
--- src/p2p/mod.rs
+++ src/p2p/mod.rs
@@ -3,13 +3,12 @@ use libp2p::core::Swarm;
 use libp2p::secio::SecioKeyPair;
 
 mod behaviour;
-mod topology;
 mod transport;
 
 /// IPFS Service
 pub struct Service {
     /// The swarm.
-    pub swarm: Swarm<transport::TTransport, behaviour::TBehaviour, topology::TTopology>
+    pub swarm: Swarm<transport::TTransport, behaviour::TBehaviour>
 }
 
 impl Service {
@@ -17,20 +16,16 @@ impl Service {
     pub fn new() -> Self {
         // Create a random key for ourselves.
         let local_key = SecioKeyPair::ed25519_generated().unwrap();
-        let local_pub_key = local_key.to_public_key();
-        let peer_id = local_pub_key.clone().into_peer_id();
+        let local_peer_id = local_key.to_peer_id();
 
         // Set up an encrypted TCP transport over the Mplex protocol.
         let transport = transport::build_transport(local_key);
 
-        // Create the topology of the network with the IPFS bootstrap nodes.
-        let topology = topology::build_topology(local_pub_key);
-
         // Create a Kademlia behaviour
-        let behaviour = behaviour::build_behaviour(peer_id);
+        let behaviour = behaviour::build_behaviour(local_peer_id.clone());
 
         // Create a Swarm
-        let swarm = Swarm::new(transport, behaviour, topology);
+        let swarm = Swarm::new(transport, behaviour, local_peer_id);
 
         Service { swarm }
     }
