b844fe17d4e128df99408fa37aaba3e2872952e8
David Craven <david@craven.ch>
2019-02-13 23:46:55 +0100

    Add examples.

---
+++ examples/client2.rs
@@ -0,0 +1,14 @@
+use futures::prelude::*;
+use ipfs::{Block, Ipfs, config::Configuration};
+
+fn main() {
+    let mut ipfs = Ipfs::from_config(Configuration::generate());
+    let block = Block::from("hello block\n");
+    ipfs.put_block(block);
+    let cid = Block::from("hello block2\n").cid();
+    let future = ipfs.get_block(cid).map(|block| {
+        println!("Received block with contents: '{:?}'",
+                 String::from_utf8_lossy(&block.data()));
+    });
+    tokio::run(ipfs.join(future).map(|_| ()));
+}
