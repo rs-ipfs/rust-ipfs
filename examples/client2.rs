ea56247defc28958d59ac3b1fbd856e6f656f621
David Craven <david@craven.ch>
2019-02-14 00:05:40 +0100

    Add logging to examples.

--- examples/client2.rs
+++ examples/client2.rs
@@ -2,6 +2,9 @@ use futures::prelude::*;
 use ipfs::{Block, Ipfs, config::Configuration};
 
 fn main() {
+    env_logger::Builder::new()
+        .parse(&std::env::var("IPFS_LOG").unwrap_or_default())
+        .init();
     let mut ipfs = Ipfs::from_config(Configuration::generate());
     let block = Block::from("hello block\n");
     ipfs.put_block(block);
