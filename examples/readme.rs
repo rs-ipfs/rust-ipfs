d12aab80a5e964d2fadefd6fb3129ad3e3766692
David Craven <david@craven.ch>
2019-02-27 19:18:58 +0100

    Update README.

--- examples/readme.rs
+++ examples/readme.rs
@@ -1,8 +1,3 @@
-# Rust IPFS implementation
-Currently implements an altruistic bitswap strategy over mdns.
-
-## Getting started
-```rust
 #![feature(async_await, await_macro, futures_api)]
 use ipfs::{Ipfs, IpfsOptions, Ipld, IpldPath, Types};
 use futures::join;
@@ -40,21 +35,3 @@ fn main() {
         ipfs.exit_daemon();
     });
 }
-```
-
-## License
-ISC License
-
-Copyright (c) 2019, David Craven and others
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
-OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
