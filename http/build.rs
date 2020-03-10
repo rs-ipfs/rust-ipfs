c3a601b90456dd68891393a116a6b666b23674b3
Joonas Koivunen <joonas@equilibrium.co>
2020-03-10 14:06:47 +0200

    add: build metadata with vergen
    this could be moved over to rust-ipfs but I think that people will
    prefer to track their own binarys version (which is tracked hopefully in
    a vcs) instead of their `rust-ipfs` deps version, which will be in their
    `Cargo.lock` file.

---
+++ http/build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // vergen will generate build flags which will allow direct usage of env!(...) macro to find
+    // VERGEN_SHA_SHORT and others: https://docs.rs/vergen/3.0.4/vergen/
+    //
+    // the rebuild on each commit can be turned off:
+    // https://docs.rs/vergen/3.0.4/vergen/struct.ConstantsFlags.html#associatedconstant.REBUILD_ON_HEAD_CHANGE
+    vergen::generate_cargo_keys(vergen::ConstantsFlags::all())
+        .expect("Unable to generate the cargo keys!");
+}
