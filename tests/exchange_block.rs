85bd2cd70513645b301e2c57eee1691367b9cbf8
Joonas Koivunen <joonas@equilibrium.co>
2020-04-01 14:01:44 +0300

    add: salvaged exchange_block
    This is from davids non-mergeable branch.
    
    Co-authored-by: David Craven <david@craven.ch>

---
+++ tests/exchange_block.rs
@@ -0,0 +1,28 @@
+use ipfs::{Block, Node};
+use libipld::cid::{Cid, Codec};
+use multihash::Sha2_256;
+
+/// Discovers a peer via mdns and exchanges a block through bitswap.
+#[async_std::test]
+async fn exchange_block() {
+    env_logger::init();
+    let mdns = false;
+
+    let data = b"hello block\n".to_vec().into_boxed_slice();
+    let cid = Cid::new_v1(Codec::Raw, Sha2_256::digest(&data));
+
+    let mut a = Node::new(mdns).await;
+    let mut b = Node::new(mdns).await;
+
+    let (_, mut addrs) = b.identity().await.unwrap();
+
+    a.connect(addrs.pop().expect("b must have address to connect to"))
+        .await
+        .unwrap();
+
+    a.put_block(Block { cid: cid.clone(), data: data.clone() }).await.unwrap();
+
+    let Block { data: data2, .. } = b.get_block(&cid).await.unwrap();
+
+    assert_eq!(data, data2);
+}
