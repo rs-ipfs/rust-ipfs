1755cd07504022f46d0dc84142e256fd29abb3f7
Joonas Koivunen <joonas@equilibrium.co>
2020-04-14 10:24:42 +0300

    feat: format string support

--- http/src/v0/refs.rs
+++ http/src/v0/refs.rs
@@ -214,18 +214,45 @@ impl RefsOptions {
 
         if self.edges {
             Ok(EdgeFormatter::Arrow)
-        } else if self.format.is_some() {
-            Err(StringError::new("format not yet implemented".into()))
+        } else if let Some(formatstr) = self.format.as_deref() {
+            let parts = parse_format(formatstr).map_err(StringError::from)?;
+            Ok(EdgeFormatter::FormatString(parts))
         } else {
             Ok(EdgeFormatter::Destination)
         }
     }
 }
 
+
 #[derive(Debug)]
 enum EdgeFormatter {
     Destination,
     Arrow,
+    FormatString(Vec<FormattedPart>)
+}
+
+/// Different parts of the format string
+#[derive(Debug, PartialEq, Eq)]
+enum FormattedPart {
+    Static(String),
+    Source,
+    Destination,
+    LinkName
+}
+
+impl FormattedPart {
+    fn format(&self, out: &mut String, src: &Cid, dst: &Cid, linkname: Option<&str>) {
+        use FormattedPart::*;
+        use fmt::Write;
+        match *self {
+            Static(ref s) => out.push_str(s),
+            Source => write!(out, "{}", src).expect("String writing shouldn't fail"),
+            Destination => write!(out, "{}", dst).expect("String writing shouldn't fail"),
+            LinkName => if let Some(s) = linkname {
+                out.push_str(s)
+            },
+        }
+    }
 }
 
 impl EdgeFormatter {
@@ -233,7 +260,93 @@ impl EdgeFormatter {
         match *self {
             EdgeFormatter::Destination => dst.to_string(),
             EdgeFormatter::Arrow => format!("{} -> {}", src, dst),
+            EdgeFormatter::FormatString(ref parts) => {
+                let mut out = String::new();
+                for part in parts {
+                    part.format(&mut out, &src, &dst, None);
+                }
+                out.shrink_to_fit();
+                out
+            },
         }
+    }
+}
+
+#[derive(Debug)]
+enum FormatError<'a> {
+    UnsupportedTag(&'a str),
+    UnterminatedTag(usize)
+}
+
+impl<'a> fmt::Display for FormatError<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        use FormatError::*;
+        match *self {
+            UnsupportedTag(tag) => write!(fmt, "unsupported tag: {:?}", tag),
+            UnterminatedTag(index) => write!(fmt, "unterminated tag at index: {}", index),
+        }
+    }
+}
+
+impl<'a> std::error::Error for FormatError<'a> {}
+
+fn parse_format(s: &str) -> Result<Vec<FormattedPart>, FormatError> {
+    use std::mem;
+
+    let mut buffer = String::new();
+    let mut ret = Vec::new();
+    let mut chars = s.char_indices();
+
+    loop {
+        match chars.next() {
+            Some((index, '<')) => {
+                if !buffer.is_empty() {
+                    ret.push(FormattedPart::Static(mem::take(&mut buffer)));
+                }
+
+                let remaining = chars.as_str();
+                let end = remaining.find('>').ok_or_else(|| FormatError::UnterminatedTag(index))?;
+
+                // the use of string indices here is ok as the angle brackets are ascii and
+                // cannot be in the middle of multibyte char boundaries
+                let inside = &remaining[0..end];
+
+                // TODO: this might need to be case insensitive
+                let part = match inside {
+                    "src" => FormattedPart::Source,
+                    "dst" => FormattedPart::Destination,
+                    "linkname" => FormattedPart::LinkName,
+                    tag => return Err(FormatError::UnsupportedTag(tag)),
+                };
+
+                ret.push(part);
+
+                // the one here is to ignore the '>', which cannot be a codepoint boundary
+                chars = remaining[end + 1..].char_indices();
+            }
+            Some((_, ch)) => buffer.push(ch),
+            None => {
+                if !buffer.is_empty() {
+                    ret.push(FormattedPart::Static(buffer));
+                }
+                return Ok(ret);
+            }
+        }
+    }
+}
+
+#[test]
+fn parse_good_formats() {
+    use FormattedPart::*;
+
+    let examples = &[
+        ("<linkname>: <src> -> <dst>", vec![LinkName, Static(": ".into()), Source, Static(" -> ".into()), Destination]),
+        ("-<linkname>", vec![Static("-".into()), LinkName]),
+        ("<linkname>-", vec![LinkName, Static("-".into())]),
+    ];
+
+    for (input, expected) in examples {
+        assert_eq!(&parse_format(input).unwrap(), expected);
     }
 }
 
