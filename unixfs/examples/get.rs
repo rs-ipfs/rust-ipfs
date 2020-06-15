c1a0943559eb714f4ae04892352ed75dbf3512a0
Joonas Koivunen <joonas@equilibrium.co>
2020-06-15 15:09:48 +0300

    wip: sneak in most of the private parts of tar-rs

--- unixfs/examples/get.rs
+++ unixfs/examples/get.rs
@@ -3,7 +3,9 @@ use ipfs_unixfs::file::{FileReadFailed};
 use std::convert::TryFrom;
 use std::fmt;
 use std::io::{Error as IoError, Read};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::io;
+use std::borrow::Cow;
 
 fn main() {
     let cid = match std::env::args().nth(1).map(Cid::try_from) {
@@ -16,7 +18,7 @@ fn main() {
             eprintln!("USAGE: {} CID\n", std::env::args().next().unwrap());
             eprintln!(
                 "Will walk the unixfs file pointed out by the CID from default go-ipfs 0.5 \
-                configuration flatfs blockstore and write all content to stdout."
+                configuration flatfs blockstore and write tar to stdout."
             );
             std::process::exit(0);
         }
@@ -55,7 +57,10 @@ fn main() {
 
 fn walk(blocks: ShardedBlockStore, start: &Cid) -> Result<(), Error> {
     use ipfs_unixfs::dir::walk::{Walker, Walk, ContinuedWalk};
-    use sha2::{Digest, Sha256};
+    use std::io::{stdout, Write};
+
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
 
     // The blockstore-specific way of reading the block. Here we assume go-ipfs 0.5 default flatfs
     // configuration, which puts the files at sharded directories and names the blocks as base32
@@ -71,52 +76,232 @@ fn walk(blocks: ShardedBlockStore, start: &Cid) -> Result<(), Error> {
 
     let mut visit = match Walker::start(&buf, &mut cache).unwrap() {
         Walk::Walker(walker) => {
-            println!("d {:?}", walker.as_entry().path());
             Some(walker)
         },
         x => unreachable!("{:?}", x),
     };
 
-    let mut hash: Option<(u64, Sha256)> = None;
+    let mut header = tar::Header::new_gnu();
+    header.set_mtime(0);
+    header.set_uid(0);
+    header.set_gid(0);
+
+    let mut long_filename_header = tar::Header::new_gnu();
+    long_filename_header.set_mtime(0);
+    long_filename_header.set_uid(0);
+    long_filename_header.set_gid(0);
+
+    let zeroes = [0; 512];
 
     while let Some(walker) = visit {
         buf.clear();
+
+        // Note: if you bind the pending or the "prefetchable", it must be dropped before the next
+        // call to continue_walk.
         let (next, _) = walker.pending_links();
         blocks.as_file(&next.to_bytes())?.read_to_end(&mut buf)?;
         visit = match walker.continue_walk(&buf, &mut cache).unwrap() {
             ContinuedWalk::File(segment, item) => {
-                let (mut total_bytes, mut hasher) = hash.take().unwrap_or_default();
-                let bytes = segment.as_ref();
-                total_bytes += bytes.len() as u64;
-                hasher.input(bytes);
 
-                if !segment.is_last() {
-                    hash = Some((total_bytes, hasher));
-                } else {
-                    let res = hasher.result();
-                    print!("f {:?} {} bytes ", item.as_entry().path(), total_bytes);
-                    for b in res {
-                        print!("{:02x}", b);
+                if segment.is_first() {
+                    // first write out the headers
+
+                    let path = item.as_entry().path();
+                    if let Err(e) = header.set_path(path) {
+                        let data = prepare_long_header(&mut header, &mut long_filename_header, path, e);
+
+                        stdout.write_all(long_filename_header.as_bytes()).unwrap();
+                        stdout.write_all(data).unwrap();
+                        stdout.write_all(&[0]).unwrap();
+
+                        let payload_bytes = data.len() + 1;
+
+                        let padding = 512 - (payload_bytes % 512);
+                        stdout.write_all(&zeroes[..padding]).unwrap();
                     }
-                    println!();
+
+                    let metadata = item.as_entry().metadata().expect("files must have metadata");
+
+                    header.set_mode(metadata.mode()
+                        .map(|mode| mode & 0o7777)
+                        .unwrap_or(0o0644));
+
+                    header.set_mtime(metadata.mtime()
+                        .and_then(|(seconds, _)| if seconds >= 0 { Some(seconds as u64) } else { None })
+                        .unwrap_or(0));
+
+                    header.set_size(segment.total_file_size());
+                    header.set_entry_type(tar::EntryType::Regular);
+                    header.set_cksum();
+
+                    stdout.write_all(header.as_bytes()).unwrap();
+                }
+
+                stdout.write_all(segment.as_ref()).unwrap();
+
+                if segment.is_last() {
+                    // write out the last data, then write padding
+
+                    let total_file_size = segment.total_file_size();
+
+                    let padding = 512 - (total_file_size % 512);
+                    stdout.write_all(&zeroes[..padding as usize]).unwrap();
                 }
 
                 item.into_inner()
             },
             ContinuedWalk::Directory(item) => {
-                println!("d {:?}", item.as_entry().path());
+
+                // create header and empty entry ... we also need to remember the last directory
+                // path not to create duplicate entries
+                let path = item.as_entry().path();
+
+                // skip empty paths; this might need to be the CID though
+                if path != Path::new("") {
+                    if let Err(e) = header.set_path(path) {
+                        let data = prepare_long_header(&mut header, &mut long_filename_header, path, e);
+
+                        stdout.write_all(long_filename_header.as_bytes()).unwrap();
+                        stdout.write_all(data).unwrap();
+                        stdout.write_all(&[0]).unwrap();
+
+                        let payload_bytes = data.len() + 1;
+
+                        let padding = 512 - (payload_bytes % 512);
+                        stdout.write_all(&zeroes[..padding]).unwrap();
+                    }
+
+                    // dirs are appended: header + 512 zeroes
+
+                    let metadata = item.as_entry().metadata().expect("files must have metadata");
+
+                    header.set_mode(metadata.mode()
+                        .map(|mode| mode & 0o7777)
+                        .unwrap_or(0o0755));
+
+                    header.set_mtime(metadata.mtime()
+                        .and_then(|(seconds, _)| if seconds >= 0 { Some(seconds as u64) } else { None })
+                        .unwrap_or(0));
+
+                    header.set_size(0);
+                    header.set_entry_type(tar::EntryType::Directory);
+                    header.set_cksum();
+
+                    stdout.write_all(header.as_bytes()).unwrap();
+                    stdout.write_all(&zeroes).unwrap();
+                }
+
                 item.into_inner()
             }
             ContinuedWalk::Symlink(_, item) => {
                 // FIXME: the symlinks seem to be prefixed with some valid unicode codepoints
-                // but need further deserialization
-                println!("s {:?} -> ???", item.as_entry().path());
+                // but need further deserialization.
+                //
+                // create symlink header and entry. I am guessing the serialization format is right
+                // away tar compatible.
+
+                // TODO: skip for now
                 item.into_inner()
             }
         };
     }
 
     Ok(())
+}
+
+
+/// Returns the raw bytes we need to write as a new entry into the tar
+fn prepare_long_header<'a>(header: &mut tar::Header, long_filename_header: &mut tar::Header, path: &'a Path, error: std::io::Error) -> &'a [u8] {
+    #[cfg(unix)]
+    fn path2bytes(p: &Path) -> &[u8] {
+        use std::os::unix::prelude::*;
+        p.as_os_str().as_bytes()
+    }
+
+    #[cfg(windows)]
+    fn path2bytes(p: &Path) -> &[u8] {
+        use std::os::windows::prelude::*;
+        p.as_os_str()
+            .to_str()
+            .expect("we should only have unicode compatible bytes even on windows")
+            .as_bytes()
+    }
+
+    #[cfg(unix)]
+    /// On unix this operation can never fail.
+    pub fn bytes2path(bytes: Cow<[u8]>) -> io::Result<Cow<Path>> {
+        use std::ffi::{OsStr, OsString};
+        use std::os::unix::prelude::*;
+
+        Ok(match bytes {
+            Cow::Borrowed(bytes) => Cow::Borrowed(Path::new(OsStr::from_bytes(bytes))),
+            Cow::Owned(bytes) => Cow::Owned(PathBuf::from(OsString::from_vec(bytes))),
+        })
+    }
+
+    #[cfg(windows)]
+    /// On windows we cannot accept non-Unicode bytes because it
+    /// is impossible to convert it to UTF-16.
+    pub fn bytes2path(bytes: Cow<[u8]>) -> io::Result<Cow<Path>> {
+        use std::ffi::{OsStr, OsString};
+        use std::os::windows::prelude::*;
+
+        return match bytes {
+            Cow::Borrowed(bytes) => {
+                let s = str::from_utf8(bytes).map_err(|_| not_unicode(bytes))?;
+                Ok(Cow::Borrowed(Path::new(s)))
+            }
+            Cow::Owned(bytes) => {
+                let s = String::from_utf8(bytes).map_err(|uerr| not_unicode(&uerr.into_bytes()))?;
+                Ok(Cow::Owned(PathBuf::from(s)))
+            }
+        };
+
+        fn not_unicode(v: &[u8]) -> io::Error {
+            other(&format!(
+                "only Unicode paths are supported on Windows: {}",
+                String::from_utf8_lossy(v)
+            ))
+        }
+    }
+
+    // we **only** have utf8 paths as protobuf has already parsed this file
+    // name and all of the previous as utf8.
+
+    let data = path2bytes(path);
+
+    let max = header.as_old().name.len();
+
+    if data.len() < max {
+        panic!("path cannot be put into tar: {:?} ({})", path, error);
+    }
+
+    {
+        let name = b"././@LongLink";
+        let gnu_header = long_filename_header.as_gnu_mut().unwrap();
+        // since we are reusing the header, zero out all of the bytes
+        let written = name.iter().copied().chain(std::iter::repeat(0)).enumerate().take(gnu_header.name.len());
+        // FIXME: there must be a better way to do this
+        for (i, b) in written {
+            gnu_header.name[i] = b;
+        }
+    }
+
+    // the plus one is documented as compliance to GNU tar, probably the null byte
+    // termination?
+    long_filename_header.set_size(data.len() as u64 + 1);
+
+    long_filename_header.set_mode(0o644);
+    long_filename_header.set_entry_type(tar::EntryType::new(b'L'));
+    long_filename_header.set_cksum();
+
+    // we still need to figure out the truncated path we put into the header
+    let path = bytes2path(Cow::Borrowed(&data[..max]))
+        .expect("quite certain we have no non-utf8 paths here");
+    header.set_path(&path)
+        .expect("we already made sure the path is of fitting length");
+
+    data
 }
 
 enum Error {
