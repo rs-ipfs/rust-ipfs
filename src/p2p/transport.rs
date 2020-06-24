83447e8ec34705a3898d90ee22345a3964468422
ljedrz <ljedrz@gmail.com>
2020-06-24 11:20:16 +0200

    feat: use noise authentication with the XX handshake
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- src/p2p/transport.rs
+++ src/p2p/transport.rs
@@ -2,9 +2,9 @@ use libp2p::core::muxing::StreamMuxerBox;
 use libp2p::core::transport::boxed::Boxed;
 use libp2p::core::transport::upgrade::Version;
 use libp2p::core::upgrade::SelectUpgrade;
-use libp2p::identity::Keypair;
+use libp2p::identity;
 use libp2p::mplex::MplexConfig;
-use libp2p::secio::SecioConfig;
+use libp2p::noise::{self, NoiseConfig};
 use libp2p::tcp::TcpConfig;
 use libp2p::yamux::Config as YamuxConfig;
 use libp2p::{PeerId, Transport};
@@ -17,11 +17,16 @@ pub(crate) type TTransport = Boxed<(PeerId, StreamMuxerBox), Error>;
 /// Builds the transport that serves as a common ground for all connections.
 ///
 /// Set up an encrypted TCP transport over the Mplex protocol.
-pub fn build_transport(key: Keypair) -> TTransport {
+pub fn build_transport(keypair: identity::Keypair) -> TTransport {
+    let xx_keypair = noise::Keypair::<noise::X25519Spec>::new()
+        .into_authentic(&keypair)
+        .unwrap();
+    let noise_config = NoiseConfig::xx(xx_keypair).into_authenticated();
+
     TcpConfig::new()
         .nodelay(true)
         .upgrade(Version::V1)
-        .authenticate(SecioConfig::new(key))
+        .authenticate(noise_config)
         .multiplex(SelectUpgrade::new(
             YamuxConfig::default(),
             MplexConfig::new(),
