cca80c433f1c1905f2aec70db576b4600c22b7b0
Joonas Koivunen <joonas@equilibrium.co>
2020-07-28 19:12:04 +0300

    doc: remove obsolete comment

--- conformance/setup.sh
+++ conformance/setup.sh
@@ -21,9 +21,6 @@ if [ -d "patches" ]; then
     # as we want to apply the patches create in a js-ipfs checkout to our node_modules
     # we'll need to remove a few leading path segments to match
     # a/packages/interface-ipfs-core/src/refs.js to node_modules/interface-ipfs-core/src/refs.js
-    #
-    # cannot use git automation any longer as it will skip any ignored file apparently,
-    # and node_modules are ignored.
     for p in patches/*; do
         echo "Applying $(basename "$p")..." >&2
         patch -d node_modules/ -p1 < "$p"
