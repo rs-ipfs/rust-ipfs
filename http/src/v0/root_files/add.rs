c59a4cbe221b973836cc8241e916a4710d71ee84
Joonas Koivunen <joonas@equilibrium.co>
2020-08-05 17:10:27 +0300

    refactor: try make /add streaming

--- http/src/v0/root_files/add.rs
+++ http/src/v0/root_files/add.rs
@@ -18,7 +18,7 @@ pub(super) async fn add_inner<T: IpfsTypes>(
     body: impl Stream<Item = Result<impl Buf, warp::Error>> + Unpin,
 ) -> Result<impl Reply, Rejection> {
     // FIXME: this should be without adder at least
-    use ipfs::unixfs::ll::file::adder::FileAdder;
+    use ipfs::unixfs::ll::{dir::builder::BufferingTreeBuilder, file::adder::FileAdder};
 
     let boundary = content_type
         .get_param("boundary")
@@ -28,9 +28,12 @@ pub(super) async fn add_inner<T: IpfsTypes>(
     let mut stream =
         MultipartStream::new(Bytes::from(boundary), body.map_ok(|mut buf| buf.to_bytes()));
 
+    // TODO: wrap-in-directory option
+    let mut tree = BufferingTreeBuilder::default();
+
     // this should be a while loop but clippy will warn if this is a while loop which will only get
     // executed once.
-    if let Some(mut field) = stream
+    while let Some(mut field) = stream
         .try_next()
         .await
         .map_err(|e| StringError::from(format!("IO error: {}", e)))?
@@ -40,6 +43,7 @@ pub(super) async fn add_inner<T: IpfsTypes>(
             .map_err(|e| StringError::from(format!("unparseable headers: {}", e)))?;
 
         if field_name != "file" {
+            // this seems constant for files and directories
             return Err(StringError::from(format!("unsupported field: {}", field_name)).into());
         }
 
@@ -48,54 +52,95 @@ pub(super) async fn add_inner<T: IpfsTypes>(
             .map_err(|e| StringError::from(format!("unparseable filename: {}", e)))?
             .to_string();
 
-        let mut adder = FileAdder::default();
-        let mut total = 0u64;
+        // unixfsv1.5 metadata seems to be in custom headers for both files and additional
+        // directories:
+        //  - mtime: timespec
+        //  - mtime-nsecs: timespec
+        //
+        // should probably read the metadata here to have it available for both files and
+        // directories?
+        //
+        // FIXME: tomorrow:
+        //  - need to make this a stream
+        //  - need to yield progress reports
+        //  - before yielding file results, we should add it to builder
+        //  - finally at the end we should build the tree
 
-        loop {
-            let next = field
-                .try_next()
-                .await
-                .map_err(|e| StringError::from(format!("IO error: {}", e)))?;
+        let content_type = field
+            .content_type()
+            .map_err(|e| StringError::from(format!("unparseable content-type: {}", e)))?;
 
-            match next {
-                Some(next) => {
-                    let mut read = 0usize;
-                    while read < next.len() {
-                        let (iter, used) = adder.push(&next.slice(read..));
-                        read += used;
+        if content_type == "application/octet-stream" {
+            // Content-Type: application/octet-stream for files
+            let mut adder = FileAdder::default();
+            let mut total = 0u64;
 
-                        let maybe_tuple = import_all(&ipfs, iter).await.map_err(|e| {
-                            StringError::from(format!("Failed to save blocks: {}", e))
-                        })?;
+            loop {
+                let next = field
+                    .try_next()
+                    .await
+                    .map_err(|e| StringError::from(format!("IO error: {}", e)))?;
 
-                        total += maybe_tuple.map(|t| t.1).unwrap_or(0);
+                match next {
+                    Some(next) => {
+                        let mut read = 0usize;
+                        while read < next.len() {
+                            let (iter, used) = adder.push(&next.slice(read..));
+                            read += used;
+
+                            let maybe_tuple = import_all(&ipfs, iter).await.map_err(|e| {
+                                StringError::from(format!("Failed to save blocks: {}", e))
+                            })?;
+
+                            total += maybe_tuple.map(|t| t.1).unwrap_or(0);
+                        }
                     }
+                    None => break,
                 }
-                None => break,
             }
-        }
 
-        let (root, subtotal) = import_all(&ipfs, adder.finish())
-            .await
-            .map_err(|e| StringError::from(format!("Failed to save blocks: {}", e)))?
-            .expect("I think there should always be something from finish -- except if the link block has just been compressed?");
+            let (root, subtotal) = import_all(&ipfs, adder.finish())
+                .await
+                .map_err(|e| StringError::from(format!("Failed to save blocks: {}", e)))?
+                .expect("I think there should always be something from finish -- except if the link block has just been compressed?");
 
-        total += subtotal;
+            total += subtotal;
 
-        let root = root.to_string();
+            // using the filename as the path since we can tolerate a single empty named file
+            // however the second one will cause issues
+            tree.put_file(filename.as_ref().unwrap_or_default(), root, total)
+                .map_err(|e| {
+                    StringError::from(format!("Failed to record file in the tree: {}", e))
+                })?;
 
-        let filename: Cow<'_, str> = if filename.is_empty() {
-            // cid needs to be repeated if no filename was given
-            Cow::Borrowed(&root)
+            let root = root.to_string();
+
+            let filename: Cow<'_, str> = if filename.is_empty() {
+                // cid needs to be repeated if no filename was given
+                Cow::Borrowed(&root)
+            } else {
+                Cow::Owned(filename)
+            };
+
+            return Ok(warp::reply::json(&Response::Added {
+                name: filename,
+                hash: Cow::Borrowed(&root),
+                size: Quoted(total),
+            }));
+        } else if content_type == "application/x-directory" {
+            // Content-Type: application/x-directory for additional directories or for setting
+            // metadata on them
+            return Err(StringError::from(format!(
+                "not implemented: {}",
+                content_type
+            )));
         } else {
-            Cow::Owned(filename)
-        };
-
-        return Ok(warp::reply::json(&Response::Added {
-            name: filename,
-            hash: Cow::Borrowed(&root),
-            size: Quoted(total),
-        }));
+            // should be 405?
+            return Err(StringError::from(format!(
+                "unsupported content-type: {}",
+                content_type
+            )));
+        }
     }
 
     Err(StringError::from("not implemented").into())
@@ -194,7 +239,7 @@ mod tests {
 
         assert_eq!(
             body,
-            r#"{"Hash":"Qma4hjFTnCasJ8PVp3mZbZK5g2vGDT4LByLJ7m8ciyRFZP","Name":"testfile.txt","Size":"20"}"#
+            "{\"Hash\":\"Qma4hjFTnCasJ8PVp3mZbZK5g2vGDT4LByLJ7m8ciyRFZP\",\"Name\":\"testfile.txt\",\"Size\":\"20\"}\r\n"
         );
     }
 
