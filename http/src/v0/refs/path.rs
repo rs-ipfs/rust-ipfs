714ecaf09399f74b5f8aa01e32a32d2afabde212
Joonas Koivunen <joonas@equilibrium.co>
2020-08-19 21:47:21 +0300

    refactor: remove http IpfsPath and tests

--- http/src/v0/refs/path.rs
+++ http/src/v0/refs/path.rs
@@ -8,128 +8,7 @@
 use cid::{self, Cid};
 use ipfs::ipld::Ipld;
 use std::collections::BTreeMap;
-use std::convert::TryFrom;
 use std::fmt;
-
-#[derive(Debug)]
-pub enum PathError {
-    InvalidCid(cid::Error),
-    InvalidPath,
-}
-
-impl fmt::Display for PathError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            PathError::InvalidCid(e) => write!(fmt, "{}", e),
-            PathError::InvalidPath => write!(fmt, "invalid path"),
-        }
-    }
-}
-
-impl std::error::Error for PathError {}
-
-/// Ipfs path following https://github.com/ipfs/go-path/
-#[derive(Debug)]
-pub struct IpfsPath {
-    /// Option to support moving the cid
-    root: Option<Cid>,
-    path: Vec<String>,
-}
-
-impl From<Cid> for IpfsPath {
-    /// Creates a new IpfsPath from just the Cid, which is the same as parsing from a string
-    /// representation of a Cid but cannot fail.
-    fn from(root: Cid) -> IpfsPath {
-        IpfsPath {
-            root: Some(root),
-            path: Vec::new(),
-        }
-    }
-}
-
-impl TryFrom<&str> for IpfsPath {
-    type Error = PathError;
-
-    fn try_from(path: &str) -> Result<Self, Self::Error> {
-        let mut split = path.splitn(2, "/ipfs/");
-        let first = split.next();
-        let (_root, path) = match first {
-            Some("") => {
-                /* started with /ipfs/ */
-                if let Some(x) = split.next() {
-                    // was /ipfs/x
-                    ("ipfs", x)
-                } else {
-                    // just the /ipfs/
-                    return Err(PathError::InvalidPath);
-                }
-            }
-            Some(x) => {
-                /* maybe didn't start with /ipfs/, need to check second */
-                if split.next().is_some() {
-                    // x/ipfs/_
-                    return Err(PathError::InvalidPath);
-                }
-
-                ("", x)
-            }
-            None => return Err(PathError::InvalidPath),
-        };
-
-        let mut split = path.splitn(2, '/');
-        let root = split
-            .next()
-            .expect("first value from splitn(2, _) must exist");
-
-        let path = split
-            .next()
-            .iter()
-            .flat_map(|s| s.split('/').filter(|s| !s.is_empty()).map(String::from))
-            .collect::<Vec<_>>();
-
-        let root = Some(Cid::try_from(root).map_err(PathError::InvalidCid)?);
-
-        Ok(IpfsPath { root, path })
-    }
-}
-
-impl IpfsPath {
-    pub fn take_root(&mut self) -> Option<Cid> {
-        self.root.take()
-    }
-
-    pub fn path(&self) -> &[String] {
-        &self.path
-    }
-
-    // Currently unused by commited code, but might become handy or easily removed later on.
-    #[allow(dead_code)]
-    pub fn debug<'a>(&'a self, current: &'a Cid) -> impl fmt::Debug + 'a {
-        struct DebuggableIpfsPath<'a> {
-            current: &'a Cid,
-            segments: &'a [String],
-        }
-
-        impl<'a> fmt::Debug for DebuggableIpfsPath<'a> {
-            fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-                write!(fmt, "{}", self.current)?;
-                if !self.segments.is_empty() {
-                    write!(fmt, "/...")?;
-                }
-
-                for seg in self.segments {
-                    write!(fmt, "/{}", seg)?;
-                }
-                Ok(())
-            }
-        }
-
-        DebuggableIpfsPath {
-            current,
-            segments: self.path.as_slice(),
-        }
-    }
-}
 
 pub fn resolve_segment(key: &str, mut ipld: Ipld) -> Result<WalkSuccess, WalkFailed> {
     ipld = match ipld {
@@ -228,91 +107,12 @@ impl std::error::Error for WalkFailed {}
 #[cfg(test)]
 mod tests {
     use super::WalkFailed;
-    use super::{resolve_segment, IpfsPath, WalkSuccess};
+    use super::{resolve_segment, WalkSuccess};
     use cid::Cid;
-    use ipfs::{ipld::Ipld, make_ipld};
+    use ipfs::{ipld::Ipld, make_ipld, IpfsPath};
     use std::convert::TryFrom;
 
     // good_paths, good_but_unsupported, bad_paths from https://github.com/ipfs/go-path/blob/master/path_test.go
-
-    #[test]
-    fn good_paths() {
-        let good = [
-            ("/ipfs/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n", 0),
-            ("/ipfs/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/a", 1),
-            (
-                "/ipfs/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/a/b/c/d/e/f",
-                6,
-            ),
-            (
-                "QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/a/b/c/d/e/f",
-                6,
-            ),
-            ("QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n", 0),
-        ];
-
-        for &(good, len) in &good {
-            let p = IpfsPath::try_from(good).unwrap();
-            assert_eq!(p.path().len(), len);
-        }
-    }
-
-    #[test]
-    fn good_but_unsupported() {
-        let unsupported = [
-            "/ipld/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n",
-            "/ipld/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/a",
-            "/ipld/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/a/b/c/d/e/f",
-            "/ipns/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/a/b/c/d/e/f",
-            "/ipns/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n",
-        ];
-
-        for &unsupported in &unsupported {
-            // these fail from failing to parse "ipld" or "ipns" as cid
-            IpfsPath::try_from(unsupported).unwrap_err();
-        }
-    }
-
-    #[test]
-    fn bad_paths() {
-        let bad = [
-            "/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n",
-            "/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/a",
-            "/ipfs/foo",
-            "/ipfs/",
-            "ipfs/",
-            "ipfs/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n",
-            "/ipld/foo",
-            "/ipld/",
-            "ipld/",
-            "ipld/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n",
-        ];
-
-        for &bad in &bad {
-            IpfsPath::try_from(bad).unwrap_err();
-        }
-    }
-
-    #[test]
-    fn trailing_slash_is_ignored() {
-        let paths = [
-            "/ipfs/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/",
-            "QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n/",
-        ];
-        for &path in &paths {
-            let p = IpfsPath::try_from(path).unwrap();
-            assert_eq!(p.path().len(), 0);
-        }
-    }
-
-    #[test]
-    fn multiple_slashes_are_deduplicated() {
-        // this is similar to behaviour in js-ipfs, as of
-        // https://github.com/ipfs-rust/rust-ipfs/pull/147/files#r408939850
-        let p =
-            IpfsPath::try_from("/ipfs/QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n///a").unwrap();
-        assert_eq!(p.path().len(), 1);
-    }
 
     fn example_doc_and_a_cid() -> (Ipld, Cid) {
         let cid = Cid::try_from("QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n").unwrap();
@@ -357,14 +157,14 @@ mod tests {
         ];
 
         for (path, expected) in &good_examples {
-            let mut p = IpfsPath::try_from(*path).unwrap();
+            let p = IpfsPath::try_from(*path).unwrap();
 
             // not really the document cid but it doesn't matter; it just needs to be !dag-pb
-            let doc_cid = p.take_root().unwrap();
+            //let doc_cid = p.take_root().unwrap();
 
             // projection
             assert_eq!(
-                walk(&doc_cid, example_doc.clone(), &p).map(|r| r.0),
+                walk(example_doc.clone(), &p).map(|r| r.0),
                 Ok(WalkSuccess::AtDestination(expected.clone()))
             );
         }
@@ -376,11 +176,10 @@ mod tests {
         let doc = make_ipld!(cid.clone());
         let path = "bafyreielwgy762ox5ndmhx6kpi6go6il3gzahz3ngagb7xw3bj3aazeita/./foobar";
 
-        let mut p = IpfsPath::try_from(path).unwrap();
-        let doc_cid = p.take_root().unwrap();
+        let p = IpfsPath::try_from(path).unwrap();
 
         assert_eq!(
-            walk(&doc_cid, doc, &p).map(|r| r.0),
+            walk(doc, &p).map(|r| r.0),
             Ok(WalkSuccess::Link(".".into(), cid))
         );
     }
@@ -391,12 +190,11 @@ mod tests {
         let doc = make_ipld!(cid);
         let path = "bafyreielwgy762ox5ndmhx6kpi6go6il3gzahz3ngagb7xw3bj3aazeita/foobar";
 
-        let mut p = IpfsPath::try_from(path).unwrap();
-        let doc_cid = p.take_root().unwrap();
+        let p = IpfsPath::try_from(path).unwrap();
 
         // go-ipfs would walk over the link even without a dot, this will probably come up with
         // dag/get
-        walk(&doc_cid, doc, &p).unwrap_err();
+        walk(doc, &p).unwrap_err();
     }
 
     #[test]
@@ -404,10 +202,9 @@ mod tests {
         let (example_doc, cid) = example_doc_and_a_cid();
 
         let path = "bafyreielwgy762ox5ndmhx6kpi6go6il3gzahz3ngagb7xw3bj3aazeita/nested/even/2/or/something_on_the_next_block";
-        let mut p = IpfsPath::try_from(path).unwrap();
-        let doc_cid = p.take_root().unwrap();
+        let p = IpfsPath::try_from(path).unwrap();
 
-        let (success, mut remaining) = walk(&doc_cid, example_doc, &p).unwrap();
+        let (success, mut remaining) = walk(example_doc, &p).unwrap();
 
         assert_eq!(success, WalkSuccess::Link("or".into(), cid));
         assert_eq!(
@@ -427,15 +224,14 @@ mod tests {
         ];
 
         for path in &mismatches {
-            let mut p = IpfsPath::try_from(*path).unwrap();
-            let doc_cid = p.take_root().unwrap();
+            let p = IpfsPath::try_from(*path).unwrap();
+            // let doc_cid = p.take_root().unwrap();
             // using just unwrap_err as the context would be quite troublesome to write
-            walk(&doc_cid, example_doc.clone(), &p).unwrap_err();
+            walk(example_doc.clone(), &p).unwrap_err();
         }
     }
 
     fn walk<'a>(
-        current: &Cid,
         mut doc: Ipld,
         path: &'a IpfsPath,
     ) -> Result<
@@ -448,6 +244,8 @@ mod tests {
         if path.path().is_empty() {
             unreachable!("empty path");
         }
+
+        let current = path.root().cid().unwrap();
 
         if current.codec() == cid::Codec::DagProtobuf {
             return Err(WalkFailed::UnsupportedWalkOnDagPbIpld);
