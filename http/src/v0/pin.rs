09f0d81b18bd13f27d6b526718b46a4c02bdeb06
Joonas Koivunen <joonas@equilibrium.co>
2020-08-27 08:54:41 +0300

    fix: use custom opt parsing, serde_json::json! output

--- http/src/v0/pin.rs
+++ http/src/v0/pin.rs
@@ -1,39 +1,15 @@
-use crate::v0::support::with_ipfs;
+use crate::v0::support::{with_ipfs, StringError};
 use futures::future::join_all;
 use ipfs::{Cid, Ipfs, IpfsTypes};
-use serde::ser::SerializeStruct;
-use serde::{Deserialize, Serialize, Serializer};
+use std::convert::TryFrom;
 use std::str::FromStr;
-use warp::{path, query, reply, Filter, Rejection, Reply};
+use warp::{reply, Filter, Rejection, Reply};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 struct AddRequest {
     args: Vec<String>,
     recursive: bool,
     progress: bool,
-}
-
-#[derive(Debug)]
-struct AddResponse {
-    pins: Vec<String>,
-    progress: usize,
-}
-
-#[derive(Debug)]
-struct InvalidCID;
-impl warp::reject::Reject for InvalidCID {}
-
-impl Serialize for AddResponse {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("AddResponse", 2)?;
-        let serialized_pins: Vec<String> = self.pins.iter().map(|x| x.to_string()).collect();
-        state.serialize_field("pins", &serialized_pins)?;
-        state.serialize_field("progress", &self.progress)?;
-        state.end()
-    }
 }
 
 async fn pin_block_request<T: IpfsTypes>(
@@ -45,27 +21,27 @@ async fn pin_block_request<T: IpfsTypes>(
         .iter()
         .map(|x| Cid::from_str(&x))
         .collect::<Result<Vec<Cid>, _>>()
-        .map_err(|_err| warp::reject::custom(InvalidCID))?;
+        .map_err(StringError::from)?;
     let dispatched_pins = cids.iter().map(|x| ipfs.pin_block(&x));
     let completed = join_all(dispatched_pins).await;
-    let pins: Vec<String> = completed
+    let pins = completed
         .iter()
         .zip(cids)
         .filter(|x| x.0.is_ok())
         .map(|x| x.1.to_string())
-        .collect();
-    let response: AddResponse = AddResponse {
-        pins,
-        progress: 100,
-    };
-    Ok(reply::json(&response))
+        .map(serde_json::Value::from)
+        .collect::<Vec<_>>();
+    Ok(reply::json(&serde_json::json!({
+        "pins": pins,
+        "progress": 100,
+    })))
 }
 
 pub fn add_pin<T: IpfsTypes>(
     ipfs: &Ipfs<T>,
 ) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
     with_ipfs(ipfs)
-        .and(query::<AddRequest>())
+        .and(add_request())
         .and_then(pin_block_request)
 }
 
@@ -77,4 +53,24 @@ pub fn list<T: IpfsTypes>(
 
 async fn list_inner<T: IpfsTypes>(ipfs: Ipfs<T>) -> Result<impl Reply, Rejection> {
     Err::<&'static str, _>(crate::v0::NotImplemented.into())
+}
+
+impl<'a> TryFrom<&'a str> for AddRequest {
+    type Error = crate::v0::support::option_parsing::ParseError<'a>;
+
+    fn try_from(q: &'a str) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+
+/// Filter to perform custom `warp::query::<AddRequest>`. This needs to be copypasted around as
+/// HRTB is not quite usable yet.
+fn add_request() -> impl Filter<Extract = (AddRequest,), Error = Rejection> + Clone {
+    warp::filters::query::raw().and_then(|q: String| {
+        let res = AddRequest::try_from(q.as_str())
+            .map_err(StringError::from)
+            .map_err(warp::reject::custom);
+
+        futures::future::ready(res)
+    })
 }
