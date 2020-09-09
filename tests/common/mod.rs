275959e301fa353f6c1c984091f95b7f90a1d07d
ljedrz <ljedrz@gmail.com>
2020-09-09 16:29:32 +0200

    test: add Topology::Star and use it

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,12 +2,18 @@ pub mod interop;
 
 use ipfs::Node;
 
+/// The way in which nodes are connected to each other; to be used with spawn_connected_nodes.
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Topology {
+    /// a > b > c
     Line,
+    /// a > b > c > a
     Ring,
+    /// a <> b <> c <> a
     Mesh,
+    /// a > b, a > c
+    Star,
 }
 
 #[allow(dead_code)]
@@ -19,25 +25,33 @@ pub async fn spawn_connected_nodes(count: usize, topology: Topology) -> Vec<Node
         nodes.push(node);
     }
 
-    if topology != Topology::Mesh {
-        for i in 0..(count - 1) {
-            nodes[i]
-                .connect(nodes[i + 1].addrs[0].clone())
-                .await
-                .unwrap();
+    match topology {
+        Topology::Line | Topology::Ring => {
+            for i in 0..(count - 1) {
+                nodes[i]
+                    .connect(nodes[i + 1].addrs[0].clone())
+                    .await
+                    .unwrap();
+            }
+            if topology == Topology::Ring {
+                nodes[count - 1]
+                    .connect(nodes[0].addrs[0].clone())
+                    .await
+                    .unwrap();
+            }
         }
-        if topology == Topology::Ring {
-            nodes[count - 1]
-                .connect(nodes[0].addrs[0].clone())
-                .await
-                .unwrap();
-        }
-    } else {
-        for i in 0..count {
-            for (j, peer) in nodes.iter().enumerate() {
-                if i != j {
-                    nodes[i].connect(peer.addrs[0].clone()).await.unwrap();
+        Topology::Mesh => {
+            for i in 0..count {
+                for (j, peer) in nodes.iter().enumerate() {
+                    if i != j {
+                        nodes[i].connect(peer.addrs[0].clone()).await.unwrap();
+                    }
                 }
+            }
+        }
+        Topology::Star => {
+            for node in nodes.iter().skip(1) {
+                nodes[0].connect(node.addrs[0].clone()).await.unwrap();
             }
         }
     }
