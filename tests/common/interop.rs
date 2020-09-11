aea5b2b3fdafa8809ef0e7e02283711dc430f301
ljedrz <ljedrz@gmail.com>
2020-09-11 13:36:50 +0200

    test: include the API port in the ForeignNode
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- tests/common/interop.rs
+++ tests/common/interop.rs
@@ -11,14 +11,13 @@ pub mod common {
     use libp2p::{core::PublicKey, Multiaddr, PeerId};
     use rand::prelude::*;
     use serde::Deserialize;
-    use std::time::Duration;
     use std::{
         env, fs,
         path::PathBuf,
         process::{Child, Command, Stdio},
-        thread,
     };
 
+    #[derive(Debug)]
     pub struct ForeignNode {
         pub dir: PathBuf,
         pub daemon: Child,
@@ -26,17 +25,21 @@ pub mod common {
         pub pk: PublicKey,
         pub addrs: Vec<Multiaddr>,
         pub binary_path: String,
+        pub api_port: u16,
     }
 
     impl ForeignNode {
         #[allow(dead_code)]
         pub fn new() -> ForeignNode {
+            use std::{io::Read, net::SocketAddr, str};
+
             // this environment variable should point to the location of the foreign ipfs binary
             #[cfg(feature = "test_go_interop")]
             const ENV_IPFS_PATH: &str = "GO_IPFS_PATH";
             #[cfg(feature = "test_js_interop")]
             const ENV_IPFS_PATH: &str = "JS_IPFS_PATH";
 
+            // obtain the path of the foreign ipfs binary from an environment variable
             let binary_path = env::vars()
                 .find(|(key, _val)| key == ENV_IPFS_PATH)
                 .unwrap_or_else(|| {
@@ -44,11 +47,13 @@ pub mod common {
                 })
                 .1;
 
+            // create the temporary directory for the repo etc
             let mut tmp_dir = env::temp_dir();
             let mut rng = rand::thread_rng();
             tmp_dir.push(&format!("ipfs_test_{}", rng.gen::<u64>()));
             let _ = fs::create_dir(&tmp_dir);
 
+            // initialize the node and assign the temporary directory to it
             Command::new(&binary_path)
                 .env("IPFS_PATH", &tmp_dir)
                 .arg("init")
@@ -60,16 +65,39 @@ pub mod common {
                 .status()
                 .unwrap();
 
-            let daemon = Command::new(&binary_path)
+            // start the ipfs daemon
+            let mut daemon = Command::new(&binary_path)
                 .env("IPFS_PATH", &tmp_dir)
                 .arg("daemon")
-                .stdout(Stdio::null())
+                .stdout(Stdio::piped())
                 .spawn()
                 .unwrap();
 
-            // give the daemon a little bit of time to start
-            thread::sleep(Duration::from_secs(1));
+            // read the stdout of the spawned daemon...
+            let mut buf = vec![0; 2048];
+            let mut index = 0;
+            if let Some(ref mut stdout) = daemon.stdout {
+                while let Ok(read) = stdout.read(&mut buf[index..]) {
+                    index += read;
+                    if str::from_utf8(&buf).unwrap().contains("Daemon is ready") {
+                        break;
+                    }
+                }
+            }
 
+            // ...so that the randomly assigned API port can be registered
+            let mut api_port = None;
+            for line in str::from_utf8(&buf).unwrap().lines() {
+                if line.contains("webui") {
+                    let addr = line.rsplitn(2, ' ').next().unwrap();
+                    let addr = addr.strip_prefix("http://").unwrap();
+                    let addr: SocketAddr = addr.rsplitn(2, '/').nth(1).unwrap().parse().unwrap();
+                    api_port = Some(addr.port());
+                }
+            }
+            let api_port = api_port.unwrap();
+
+            // run /id in order to register the PeerId, PublicKey and Multiaddrs assigned to the node
             let node_id = Command::new(&binary_path)
                 .env("IPFS_PATH", &tmp_dir)
                 .arg("id")
@@ -98,6 +126,7 @@ pub mod common {
                 pk,
                 addrs: addresses,
                 binary_path,
+                api_port,
             }
         }
 
