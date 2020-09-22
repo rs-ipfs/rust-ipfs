af7d88b42155aeb02083e99ad6bc1e7bbd9ae550
ljedrz <ljedrz@gmail.com>
2020-09-22 10:33:38 +0200

    fix: remove the unimplemented ipns functionalities from ipns
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- src/ipns/mod.rs
+++ src/ipns/mod.rs
@@ -3,7 +3,6 @@ use crate::error::Error;
 use crate::path::{IpfsPath, PathRoot};
 use crate::repo::RepoTypes;
 use crate::Ipfs;
-use libp2p::PeerId;
 
 mod dnslink;
 mod entry;
@@ -26,27 +25,8 @@ impl<Types: RepoTypes> Ipns<Types> {
         let path = path.to_owned();
         match path.root() {
             PathRoot::Ipld(_) => Ok(path),
-            PathRoot::Ipns(peer_id) => match self.ipfs.repo.get_ipns(peer_id).await? {
-                Some(path) => Ok(path),
-                None => Err(anyhow::anyhow!("unimplemented")),
-            },
+            PathRoot::Ipns(_) => Err(anyhow::anyhow!("unimplemented")),
             PathRoot::Dns(domain) => Ok(dnslink::resolve(domain).await?),
         }
-    }
-
-    /// Publishes an ipld path.
-    pub async fn publish(&self, key: &PeerId, path: &IpfsPath) -> Result<IpfsPath, Error> {
-        let future = self.ipfs.repo.put_ipns(key, path);
-        let key = key.to_owned();
-        let mut path = path.to_owned();
-        future.await?;
-        path.set_root(PathRoot::Ipns(key));
-        Ok(path)
-    }
-
-    /// Cancel an ipns path.
-    pub async fn cancel(&self, key: &PeerId) -> Result<(), Error> {
-        self.ipfs.repo.remove_ipns(key).await?;
-        Ok(())
     }
 }
