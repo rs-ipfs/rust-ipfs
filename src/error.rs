91c0bff5fb0ced0a91abffe020edf974d2b50b93
Joonas Koivunen <joonas@equilibrium.co>
2020-09-23 11:32:07 +0300

    doc: learn about ipfs::error::TryError, document it

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,17 @@
+///! IPFS crate wide errors.
+///!
+///! The error handling in `ipfs` is subject to change in the future.
+
+/// Just re-export anyhow for now.
+///
+/// # Stability
+///
+/// Very likely to change in the future.
 pub use anyhow::Error;
 
+/// A try conversion failed.
+///
+/// # Stability
+///
+/// Very likely to change in the future.
 pub struct TryError;
