3e8cf550a8a68be51d737dd5f45e5c53bedb1949
fetchadd <kaizengliu@qq.com>
2020-12-23 18:22:50 +0800

    refactor: adjust to cargo fmt

--- src/repo/kv.rs
+++ src/repo/kv.rs
@@ -159,7 +159,7 @@ impl PinStore for KvDataStore {
             let is_already_pinned = is_pinned(self, target);
 
             if let Ok(true) = is_already_pinned {
-                continue
+                continue;
             }
 
             // value is for get information like "Qmd9WDTA2Kph4MKiDDiaZdiB4HJQpKcxjnJQfQmM5rHhYK indirect through QmXr1XZBg1CQv17BPvSWRmM7916R6NLL7jt19rhCPdVhc5"
